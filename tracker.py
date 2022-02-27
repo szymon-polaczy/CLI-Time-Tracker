33c428ac3715afa36e8808703eeb71fb835d2119
Szymon <thomas_frey@tutanota.com>
2022-02-27 13:25:51 +0100

    Some kind of a plan of how the md file will look

---
+++ tracker.py
@@ -0,0 +1,19 @@
+"""
+MD FILE:
+
+# START - Project Name
+
+## START - Task Name
+
+### Time - 18/03/2022 - 18:50:30 - Written how the project is gona look
+
+## END - Task Name
+
+## START - Task Name
+
+### Time - 18/03/2022 - 18:50:30 - NONE (When you don't want to comment anything)
+
+## END - Task Name
+
+# END - Project Name
+"""
